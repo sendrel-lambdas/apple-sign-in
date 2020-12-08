a336b4d1f9912c8a5ac8ef314dd6794c45645ca9
Sendrel Van Parate <sendrel@lambdas.io>
2020-12-08 17:11:06 +0800

    added callback

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,7 +3,10 @@ import AppleLogin from 'react-apple-login';
 const clientId = process.env.REACT_APP_APPLE_CLIENT_ID as string;
 const redirectURI = process.env.REACT_APP_APPLE_REDIRECT_URI as string;
 const SignIn: React.FC = () => {
-  return <AppleLogin clientId={clientId} redirectURI={redirectURI} />;
+  const handleCallback = (e: any) => {
+    console.log(e);
+  };
+  return <AppleLogin clientId={clientId} redirectURI={redirectURI} callback={handleCallback} />;
 };
 
 export default SignIn;
