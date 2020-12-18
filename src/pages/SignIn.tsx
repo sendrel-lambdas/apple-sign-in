8a54de9673a2af91f79f3678e21f2a879474dc75
Sendrel Van Parate <sendrel@lambdas.io>
2020-12-18 11:39:21 +0800

    new version

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import AppleLogin from 'react-apple-login';
+import { Helmet } from 'react-helmet';
+import * as scriptjs from 'scriptjs';
 const clientId = process.env.REACT_APP_APPLE_CLIENT_ID as string;
 const redirectURI = process.env.REACT_APP_APPLE_REDIRECT_URI as string;
-const SignIn: React.FC = () => {
-  const handleCallback = (e: any) => {
-    console.log(e);
+
+declare global {
+  interface Window {
+    AppleID: any;
+  }
+}
+
+scriptjs.get('https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js', () => {
+  const params = {
+    clientId: process.env.REACT_APP_APPLE_CLIENT_ID,
+    redirectURI: process.env.REACT_APP_APPLE_REDIRECT_URI,
+    scope: 'name email',
   };
-  return <AppleLogin clientId={clientId} redirectURI={redirectURI} callback={handleCallback} scope="name email" />;
+  //
+  window.AppleID.auth.init(params);
+});
+const SignIn: React.FC = () => {
+  return (
+    <React.Fragment>
+      <button onClick={() => window.AppleID.auth.signIn()} />
+    </React.Fragment>
+  );
 };
 
 export default SignIn;
